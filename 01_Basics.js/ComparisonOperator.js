b4934172090ac6f6dd059f3aaa759fb34edc6c21
Tejas Suresh Vispute <104120823+TejaVispute@users.noreply.github.com>
2023-12-30 16:18:02 +0000

    changes added

---
+++ 01_Basics.js/ComparisonOperator.js
@@ -0,0 +1,17 @@
+// console.log(2!=10);
+
+// console.log("1"==1)
+// console.log("1"===1)
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined<=0);
+
+// strictly check === check value and datatype
+
+
+
---
+++ 01_Basics.js/ConversionOperatons.js
@@ -0,0 +1,14 @@
+
+// console.log("1" +1)
+
+// console.log(1+1+"1" +1)
+
+// console.log("1"+2+2)
+
+// console.log(2+2+"1")
+
+// console.log(+true);
+// console.log(+"");
+let gameCounter=100;
+gameCounter++
+console.log(gameCounter);
