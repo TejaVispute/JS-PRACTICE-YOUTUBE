74ff2e350caa213cd25eca7f9864f419fa2d0133
Tejas Suresh Vispute <104120823+TejaVispute@users.noreply.github.com>
2023-12-31 10:04:56 +0000

    Number math code pushed

---
+++ 01_Basics.js/06_Numbers_Math.js
@@ -0,0 +1,52 @@
+let number= 100;
+
+let score = new Number(99.2222);
+
+// console.log(number);
+// console.log(score);
+
+// console.log(score.toString()
+// console.log(score.toFixed(2))
+
+let hundreds = 1000000;
+
+// console.log(hundreds.toLocaleString("en-IN"));
+
+
+// -------------------Maths----------------------
+
+// console.log(Math);
+
+// make negative value positive
+// console.log(Math.abs(-4))
+
+// make value roundoff according to point value greater or smaller
+// console.log(Math.round(7.4))
+
+// it will take top value ex 4.2 is ther it will output 5
+// console.log(Math.ceil(4.2))
+
+// it will take bottom value 4.2 is there it will ouput 4
+// console.log(Math.floor(4.2));
+
+// minimum and maximum value in array using min and max
+
+// console.log(Math.min(0,1,2,3,4,7,71,));
+// console.log(Math.max(0,1,2,3,4,7,71,));
+
+
+console.log(Math.floor(Math.random()*10)+1)
+
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
