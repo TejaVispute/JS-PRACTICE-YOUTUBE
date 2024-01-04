03dbddee3ec752389a6b280740dfc7a51e8e0a07
Tejas Suresh Vispute <104120823+TejaVispute@users.noreply.github.com>
2024-01-04 15:01:52 +0000

    added dates code

---
+++ 01_Basics.js/07_datesInJS.js
@@ -0,0 +1,32 @@
+let myDate = new Date();
+
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toJSON())
+
+
+let myCreatedDate=new Date(2024,0,2,21,27);
+// console.log(myCreatedDate.toLocaleString());
+
+// timestamps
+const timeStamp = Date.now();
+
+// console.log(Math.floor((timeStamp)/1000))
+
+// console.log(new Date)
+
+const date = new Date();
+// console.log(date.getFullYear());
+console.log(date.getMonth()+1);
+console.log(date.getDay())
+
+
+
+
+
+
+
+
+
