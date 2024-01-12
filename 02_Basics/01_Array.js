decc0269152c5a2b82e83d4caa3a8cbbf3521512
Tejas Suresh Vispute <104120823+TejaVispute@users.noreply.github.com>
2024-01-12 15:16:09 +0000

    added code

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,27 @@
+// let Arr= [1,2,3,4,5,6];
+
+// Arr.push(7);
+
+// Arr.unshift(99)
+
+// console.log(Arr)
+// console.log(Arr.includes(7));
+// console.log(Arr.indexOf(99));
+
+// let newArr= Arr.join(" ");
+
+// console.log(newArr);
+
+
+// slice mathod
+
+// let newArr= Arr.slice(0,2);
+// console.log(Arr);
+// console.log(newArr);
+
+// console.log(Arr);
+
+// let newArr2= Arr.splice(0,4);
+// console.log(Arr);
+// console.log(newArr2);
+
