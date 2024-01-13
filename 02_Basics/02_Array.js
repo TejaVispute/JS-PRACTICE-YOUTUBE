1b2cde774f13e9eea776ad4121f3c77e75c9b9b2
Tejas Suresh Vispute <104120823+TejaVispute@users.noreply.github.com>
2024-01-13 15:37:29 +0000

    array end

---
+++ 02_Basics/02_Array.js
@@ -0,0 +1,35 @@
+// let marvelHeros= ["Captaim", "BlackPanther","Ironman" ,"Hulk"];
+
+// let dcHeros=["flash","superman","batman"];
+
+// let mergeHeros = marvelHeros.concat(dcHeros)
+// console.log(mergeHeros)
+
+// marvelHeros.push(dcHeros)
+
+// console.log(marvelHeros);
+
+// let allNewHeros=[...marvelHeros,...dcHeros];
+// console.log(allNewHeros);
+
+let array= [1,2,3,[10,22],20,22,[5,7,8,[9,4]]];
+
+// let output=array.flat(Infinity)
+
+// console.log(output);
+
+// console.log(Array.isArray("Teja"));
+// console.log(Array.from("Tejas"));
+// console.log(Array.from({name:"Tejas"}));
+
+let score1=100;
+let score2=200;
+let score3=300;
+
+
+console.log(Array.of(score1,score2,score3))
+
+
+
+
+
